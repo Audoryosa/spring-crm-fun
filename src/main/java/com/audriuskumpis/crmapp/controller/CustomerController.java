886a7eab2ab46bcbf1c94a668dfd9aab2e0eb2f0
Audoryosa <anoimas@gmail.com>
2020-08-09 21:33:43 +0300

    base

--- src/main/java/com/audriuskumpis/crmapp/controller/CustomerController.java
+++ src/main/java/com/audriuskumpis/crmapp/controller/CustomerController.java
@@ -1,15 +1,26 @@
 package com.audriuskumpis.crmapp.controller;
 
+import com.audriuskumpis.crmapp.entity.Customer;
+import com.audriuskumpis.dao.CustomerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model model){
+        List<Customer> customers = customerDAO.getCustomers();
+        model.addAttribute("customers", customers);
         return "list-customers";
     }
 }
---
+++ src/main/java/com/audriuskumpis/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.audriuskumpis.dao;
+
+import com.audriuskumpis.crmapp.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
