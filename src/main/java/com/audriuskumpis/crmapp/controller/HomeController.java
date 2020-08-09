39b0f66892993b4a6ff00e09cd990c45186a3877
Audoryosa <anoimas@gmail.com>
2020-08-09 15:36:04 +0300

    base

---
+++ src/main/java/com/audriuskumpis/crmapp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.audriuskumpis.crmapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello!</title>
+    <link href="/css/main.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Coming soon: some site</h1>
+    <script src="/js/main.js"></script>
+</body>
+</html>
