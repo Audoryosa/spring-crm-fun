43ea33a981008bbdfc0e0a61aa78e267deef3ef2
Audoryosa <anoimas@gmail.com>
2020-09-17 20:48:59 +0300

    show roles on login + bug fixes

--- src/main/java/com/audriuskumpis/crmapp/config/SecurityConfig.java
+++ src/main/java/com/audriuskumpis/crmapp/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.audriuskumpis.crmapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionOperations;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebSecurityExpressionRoot;
 
 @Configuration
 @EnableWebSecurity
@@ -33,5 +39,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.expressionHandler(new DefaultWebSecurityExpressionHandler() {
+            @Override
+            protected SecurityExpressionOperations createSecurityExpressionRoot(Authentication authentication, FilterInvocation fi) {
+                WebSecurityExpressionRoot root = (WebSecurityExpressionRoot) super.createSecurityExpressionRoot(authentication, fi);
+                root.setDefaultRolePrefix("");
+                return root;
+            }
+        });
     }
 }
--- src/main/webapp/WEB-INF/jsp/public/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/public/hello.jsp
@@ -1,5 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fomr" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%@ page import="com.audriuskumpis.crmapp.Messages" %>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +10,11 @@
 </head>
 <body>
 <h1 class="hello-title">Welcome to customer registration site!</h1>
+<h4>Signed in as: <security:authentication property="principal.username"/> | <security:authentication
+        property="principal.authorities"/></h4>
 
 <a href="${pageContext.request.contextPath}/customer/list" class="btn btn-primary">
     <c:out value="${Messages.NEXT_PAGE}"/></a>
-
-<a href="${pageContext.request.contextPath}/login" class="btn btn-primary">
-    <c:out value="${Messages.LOGIN}"/></a>
 
 <fomr:form action="${pageContext.request.contextPath}/logout" method="POST">
     <input class="btn btn-secondary margin-10-top-bottom" type="submit" value="${Messages.LOGOUT}"/>
